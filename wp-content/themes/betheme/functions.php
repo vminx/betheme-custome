e9c8c1abd23c3bd8f145238fa6ea1deffb5308b7
vminx <470132216@qq.com>
2018-11-09 14:17:32 +0800

    FN0001  |   当前网站不需要 FN0002  |   移除一些无用的功能

--- wp-content/themes/betheme/functions.php
+++ wp-content/themes/betheme/functions.php
@@ -43,7 +43,8 @@ load_theme_textdomain( 'mfn-opts', LANG_DIR );	// backend
 if( ! function_exists( 'mfn_admin_scripts' ) )
 {
 	function mfn_admin_scripts() {
-		wp_enqueue_script( 'jquery-ui-sortable' );
+		// 改动编号：FN0001  |   当前网站不需要
+		// wp_enqueue_script( 'jquery-ui-sortable' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'mfn_admin_scripts' );
@@ -195,4 +196,12 @@ if( is_admin() ){
 @ini_set( 'max_execution_time', '300' );
 
 
-
+// 改动编号：FN0002  |   移除一些无用的功能
+// Remove Actions
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+remove_action( 'wp_head', 'rel_canonical' );
+remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
